74023a26c96668aa2f25003f4ce7e1659b8abd67
Timeless <leongandy027@gmail.com>
2023-05-08 01:31:24 +0800

    init v3

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,17 +1,20 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react"
 import type { AppProps } from "next/app";
+import { theme } from "../chakra/theme";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "../graphql/apollo-client";
 
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-
-    <SessionProvider session={session}>
-
-      <Component {...pageProps} />
-
-
-    </SessionProvider>
-
+    <ApolloProvider client = {client}>
+      <SessionProvider session={session}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SessionProvider>
+    </ApolloProvider>
   );
 }
 
