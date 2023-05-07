8da407cd4dbae9724d001ade80e5f5088eb474bd
TimelesClock <leongandy027@gmail.com>
2023-05-07 19:23:06 +0800

    initial'

---
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+//Prisma is acting as a middleman between nextJS app and the mongoDB
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+        //Note: using "as" keyword to type cast is not recommended
+        //as it is overwriting the TS compiler which defeats the purpose of TS
+        //but it is used in this case as we know the env is a string
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+});
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type {NextPage} from "next";
+import { signIn, signOut, useSession } from "next-auth/react"
+
+const Home:NextPage = () => {
+  const { data } = useSession();
+  console.log("here is the data", data)
+
+  return (
+    <>
+      {data?.user ? (
+        <button onClick={()=>signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={()=>signIn("google")}>Sign In</button>
+      )}
+      {data?.user?.name}
+    </>
+  );
+};
+
+export default Home;
